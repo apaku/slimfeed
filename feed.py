8439aa2ea46176a872b9721bfaec38cdc7482b65
Andreas Pakulat <apaku@gmx.de>
2011-02-13 12:00:39 +0100

    Boilerplate code for feeds.
    These are just skeletons for the classes with some basic API and code to
    load/store and add feeds.

---
+++ feed.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+#    Copyright 2011 by Andreas Pakulat <apaku@gmx.de>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+#    02110-1301  USA.
+
+class Feed:
+    def __init__(self):
+        self._entries = []
+
+    def load(self, store):
+        pass
+
+    def save(self, store):
+        for entry in self._entries:
+            entry.save(store)
+
+    def entries(self):
+        return self._entries
+
+    def addEntry(self, entry):
+        if not entry in self._entries:
+            self._entries.append(entry)
+
+    def removeEntry(self, entry):
+        if entry in self._entries:
+            self._entries.remove(entry)
+
+if __name__ == "__main__":
+    import sys
+    print "Cannot run this module"
+    sys.exit(1)
+
